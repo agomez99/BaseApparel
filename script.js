18d0c7847b86c439f3e8af1bb701a2ce510a25ed
Austine Gomez <austine.gomez@yahoo.com>
2023-07-11 22:52:34 -0500

    FUNCTIONAL

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Hello!');
+function isValidEmailFormat(emailAddress) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(emailAddress) && emailAddress !== "";
+  }
+  
+  function signUp() {
+    const email = document.getElementById("email").value;
+    const errorDisplay = document.getElementById("error-display");
+    const successDisplay = document.getElementById("success-display");
+  
+    if (!isValidEmailFormat(email)) {
+      errorDisplay.classList.add("visible");
+      errorDisplay.textContent = "Please provide a valid email";
+      document.getElementById("email").style.border = "1px solid red";
+      return;
+    }
+  
+    errorDisplay.classList.remove("visible");
+    errorDisplay.textContent = "";
+  
+    console.log("Signed up with email:", email);
+  
+    document.getElementById("email").value = "";
+  
+    const message = "Successfully signed up newsletter for ";
+    successDisplay.style.fontWeight = "bold";
+    successDisplay.textContent = message + email;
+    successDisplay.classList.add("visible");
+  }
+  
+  function clearError() {
+    const errorDisplay = document.getElementById("error-display");
+    const successDisplay = document.getElementById("success-display");
+  
+    errorDisplay.textContent = "";
+    successDisplay.textContent = "";
+    document.getElementById("email").style = "";
+  }
+  
