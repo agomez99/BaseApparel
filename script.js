9c05b2c21a7b34ff438aef4e0dad5091c6ef76f6
Austine Gomez <austine.gomez@yahoo.com>
2023-07-12 22:41:43 -0500

    complete

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-function isValidEmailFormat(emailAddress) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(emailAddress) && emailAddress !== "";
-  }
-  
-  function signUp() {
-    const email = document.getElementById("email").value;
-    const errorDisplay = document.getElementById("error-display");
-    const successDisplay = document.getElementById("success-display");
-  
-    if (!isValidEmailFormat(email)) {
-      errorDisplay.classList.add("visible");
-      errorDisplay.textContent = "Please provide a valid email";
-      document.getElementById("email").style.border = "1px solid red";
-      document.getElementById("error-icon").style.visibility = "visible";
-      return;
-    }
-  
-    errorDisplay.classList.remove("visible");
-    errorDisplay.textContent = "";
-  
-    console.log("Signed up with email:", email);
-  
-    document.getElementById("email").value = "";
-  
-    const message = "Successfully signed up newsletter for ";
-    successDisplay.style.fontWeight = "bold";
-    successDisplay.textContent = message + email;
-    successDisplay.classList.add("visible");
-  }
-  
-  function clearError() {
-    const errorDisplay = document.getElementById("error-display");
-    const successDisplay = document.getElementById("success-display");
-  
-    errorDisplay.textContent = "";
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    successDisplay.textContent = "";
-    document.getElementById("email").style = "";
-    document.getElementById("error-icon").style.visibility = "hidden";
+function isValidEmailFormat(emailAddress) {
+  return emailRegex.test(emailAddress) && Boolean(emailAddress);
+}
+
+function signUp() {
+  const emailInput = document.getElementById("email");
+  const errorDisplay = document.getElementById("error-display");
+  const successDisplay = document.getElementById("success-display");
+
+  const email = emailInput.value;
+
+  if (!isValidEmailFormat(email)) {
+    errorDisplay.classList.add("visible");
+    errorDisplay.textContent = "Please provide a valid email";
+    emailInput.style.border = "2px solid hsl(0, 93%, 68%)";
+    document.getElementById("error-icon").style.visibility = "visible";
+    return;
   }
-  
+
+  errorDisplay.classList.remove("visible");
+  errorDisplay.textContent = "";
+
+  console.log("Signed up with email:", email);
+
+  emailInput.value = "";
+
+  const message = "Successfully signed up newsletter for ";
+  successDisplay.style.fontWeight = "bold";
+  successDisplay.textContent = message + email;
+  successDisplay.classList.add("visible");
+}
+
+function clearError() {
+  const errorDisplay = document.getElementById("error-display");
+  const successDisplay = document.getElementById("success-display");
+  const emailInput = document.getElementById("email");
+
+  errorDisplay.textContent = "";
+  successDisplay.textContent = "";
+  emailInput.style = "";
+  document.getElementById("error-icon").style.visibility = "hidden";
+}
